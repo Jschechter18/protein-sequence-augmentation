c58c4a079f9fd2a7d625a8280c70d22596a2e0d0
ashleygya03 <ashleygya03@gwu.edu>
2026-06-14 18:03:50 -0400

    update Code package in script

--- code/src/models/esm2.py
+++ code/src/models/esm2.py
@@ -15,7 +15,7 @@ import torch.optim as optim
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 
-from code.src.utils.dataloader import create_dataloader
+from Code.src.utils.dataloader import create_dataloader
 
 
 logging.basicConfig(level=logging.INFO)
